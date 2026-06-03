d4e07ffa65a7c031107ede18487b9f48f4dee203
rajkavyanjali5-ship-it <rajkavyanjali5@gmail.com>
2026-06-03 18:45:37 +0530

    Create Week_1/Day_2/ContainsDuplicate.java

---
+++ Week_1/Day_2/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (numbers.contains(num)) {
+                return true;
+            }
+
+            numbers.add(num);
+        }
+
+        return false;
+    }
+}
