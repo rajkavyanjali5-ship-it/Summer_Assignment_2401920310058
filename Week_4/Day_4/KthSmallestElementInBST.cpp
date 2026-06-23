7be4071c4e2feff8679037ab7c3a192bb534fa65
rajkavyanjali5-ship-it <rajkavyanjali5@gmail.com>
2026-06-23 08:32:53 +0530

    Implement Kth smallest element in BST

---
+++ Week_4/Day_4/KthSmallestElementInBST.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int count = 0;
+    int answer = 0;
+
+    void inorder(TreeNode* root, int k) {
+
+        if(root == NULL)
+            return;
+
+        inorder(root->left, k);
+
+        count++;
+
+        if(count == k) {
+            answer = root->val;
+            return;
+        }
+
+        inorder(root->right, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+
+        inorder(root, k);
+        return answer;
+    }
+};
